4686cd1d2ac33c60aeaf25734d9af869551b6f49
libinsocxo <libin@socxo.com>
2024-02-07 11:16:17 +0530

    the initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogyWebapi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
